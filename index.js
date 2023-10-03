473a0aa0435b470a528633f1108c2460b1fb1a9a
Aliyev-Orkhan <orxannaliyevvv@gmail.com>
2023-10-03 10:16:24 +0400

    java1

---
+++ index.js
@@ -0,0 +1,46 @@
+var q1 = prompt ("Bir   Kərə Yüksələn Bayraq Bir Daha Enməz İfadəsi Hansı Şəxsiyyətə Aiddir?") ;
+if (q1 == "Məmməd Əmin Resulzadəyə") {
+    console.log("Doğrudur");
+    alert("Doğrudur");   
+}else{
+    console.log("Yalnışdır")
+        alert("Yalnışdır");
+}
+var q2 = prompt ("Bakıda və Müsəlman Şərqində ilk Qızlar Məktəbi Kimin Təşəbbüsüylə İnşa Edilmişdir?");
+
+if (q2 == " Haci Zeynalabdin Tağıyevin") {
+
+console.log("Doğrudur");
+alert("Doğrudur")
+}else{
+    console.log("Yalnışdır")
+        alert("Yalnışdır");
+}
+var q3 = prompt ("Azərbaycanın ilk Diplomat Qadını Sayılan Şəxs Kimdir")
+if (q3 =="Sara Xatun") {
+    console.log("Doğrudur")
+    alert("Doğrudur")
+}else{
+    console.log("Yalnışdır")
+    alert("Yalnışdır")
+
+}
+    
+var q4 = prompt ("Azərbaycanda Neçə Ədəd Mülki Hava Limanı Vardır?");
+if (q4 == "12 Ədəd"){
+    console.log("Doğrudur");
+    alert("Doğrudur")
+}else{
+    console.log("Yalnışdır")
+        alert("Yalnışdır");
+}
+
+var q5 = prompt ("Tarixdə Ən böyük Azərbaycan Dövləti?");
+if (q5 == "Səfəvilər Dövləti"){
+    console.log("Doğrudur");
+    alert("Doğrudur") 
+}else{
+    console.log("Yalnışdır")
+        alert("Yalnışdır");
+}
+
